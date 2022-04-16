20c4ec0959946ff1717d06da8fcafb2dd48ef432
xarantolus <xarantolus@protonmail.com>
2022-04-16 14:57:55 +0200

    Database scheme

--- database/create_database_scheme.sql
+++ database/create_database_scheme.sql
@@ -1,2 +1,50 @@
-create table Repository ( id int primary key, username varchar not null, name varchar(100) not null);
+create table Repository (
+    id int primary key,
+    username varchar not null,
+    name varchar(100) not null
+);
 
+create table RepoStats (
+    repo_id int REFERENCES Repository,
+    date date not null default CURRENT_DATE,
+    stars int not null,
+    forks int not null,
+    size int not null,
+    subscribers int not null,
+    primary key (repo_id, date)
+);
+
+create table RepoTrafficViews (
+    repo_id int REFERENCES Repository,
+    date date not null default CURRENT_DATE,
+    count int not null,
+    uniques int not null,
+    primary key (repo_id, date)
+);
+
+create table RepoTrafficClones (
+    repo_id int REFERENCES Repository,
+    date date not null default CURRENT_DATE,
+    count int not null,
+    uniques int not null,
+    primary key (repo_id, date)
+);
+
+create table RepoTrafficPaths (
+    repo_id int REFERENCES Repository,
+    date date not null default CURRENT_DATE,
+    path text not null,
+    title text not null,
+    count int not null,
+    uniques int not null,
+    primary key (repo_id, date, path)
+);
+
+create table RepoTrafficReferrers (
+    repo_id int REFERENCES Repository,
+    date date not null default CURRENT_DATE,
+    referrer text not null,
+    count int not null,
+    uniques int not null,
+    primary key (repo_id, date, referrer)
+);
